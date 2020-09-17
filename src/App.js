915b6ad327be29690976ad0a25a19735dde41803
freefl <freefl@gmail.com>
2020-09-17 17:35:23 -0400

    working verison

--- src/App.js
+++ src/App.js
@@ -2,22 +2,28 @@ import React , {Component } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import { Home} from './Home';
 import { About} from './About';
-
+import { NoMatch} from './NoMatch';
+import {Layout} from './components/Layout';
+import {NavigationBar} from './components/NavigationBar';
+import {Jumbotron} from './components/Jumbotron';
 class App extends Component {
   render(){
     return (
       <React.Fragment>
+        <NavigationBar />
+        <Jumbotron />
+        <Layout></Layout>
         <Router>
-         <Switch>
-           <Route exact path="/" component = {Home} />
-           <Route path="/about" component = {About} />
-           </Switch> 
-        </Router>
-      </React.Fragment>
+          <Switch>
+            <Route exact path="/" component = {Home} />
+            <Route path="/about" component = {About} />
+            <Route component = {NoMatch} />
+          </Switch> 
+      </Router>
+   </React.Fragment>
+
     );
   }
-
-
-
 }
+
 export default App;
